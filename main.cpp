265d89fce3799688711fe6fb6b1eefe74759b6a0
Damon <warrenonedge@gmail.com>
2014-03-06 18:30:54 -0500

    Main game screen added. John don't hate me.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main()
 {
     // Create the main window
     sf::RenderWindow app(sf::VideoMode(1000, 600), "SFML window");
-
-    // Load a sprite to display
+//-----------------------------------------------------------------
+    //Main Screen Start Objects
     sf::Texture texture;
     if (!texture.loadFromFile("GameMainScreen.bmp"))
         return EXIT_FAILURE;
@@ -19,60 +19,128 @@ int main()
     {
         return EXIT_FAILURE; // error...
     }
-    sf::Text text;
-    text.setFont(font);
-    text.setString("Start");
-    text.setCharacterSize(24);
-    text.setColor(sf::Color::White);
-    int textx = 500-(text.getGlobalBounds().width/2);
-    int texty = 460-(text.getGlobalBounds().height/2);
-    text.setPosition(textx,texty);
+    sf::Text startText("Start",font,24);
+    startText.setColor(sf::Color::White);
+    startText.setStyle(sf::Text::Underlined);
+    int startTextx = 500-(startText.getGlobalBounds().width/2);
+    int startTexty = 460-(startText.getGlobalBounds().height/2);
+    startText.setPosition(startTextx,startTexty);
+    //Main Screen Objects End
+//-----------------------------------------------------------------
+    //Game Screen Objects Start
+    sf::CircleShape ball(37);
+    ball.setPosition(87,476);
+    ball.setFillColor(sf::Color::Red);
+    sf::Text massText("Mass:",font,20);
+    massText.setColor(sf::Color::White);
+    massText.setPosition(62,225);
 
+    sf::Text velocityText("Velocity:",font,20);
+    velocityText.setColor(sf::Color::White);
+    velocityText.setPosition(62,300);
+
+    sf::Text angleText("Angle:",font,20);
+    angleText.setColor(sf::Color::White);
+    angleText.setPosition(62,375);
+
+    sf::Text replayText("Replay",font,20);
+    replayText.setColor(sf::Color::White);
+    replayText.setStyle(sf::Text::Underlined);
+    int replayx = 62;
+    int replayy = 75;
+    replayText.setPosition(replayx,replayy);
+    //Game Screen Objects End
+//-----------------------------------------------------------------
+    bool mainScreen = true;
 	// Start the game loop
     while (app.isOpen())
     {
-        // Process events
-        sf::Event event;
-        while (app.pollEvent(event))
+        if (mainScreen == true) //Main Screen Display
         {
-            // Close window : exit
-            if (event.type == sf::Event::Closed)
-                app.close();
-            if (event.type == sf::Event::MouseMoved)
+            // Process events
+            sf::Event event;
+            while (app.pollEvent(event))
             {
-                int mousex = event.mouseMove.x;
-                int mousey = event.mouseMove.y;
-                if (active(textx,texty,text.getLocalBounds().width,text.getLocalBounds().height,mousex,mousey))
-
+                // Close window : exit
+                if (event.type == sf::Event::Closed)
+                    app.close();
+                if (event.type == sf::Event::MouseMoved)
                 {
-                    text.setColor(sf::Color::Red);
-                }
-                else
-                {
-                    text.setColor(sf::Color::White);
-                }
-            }
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == sf::Mouse::Left)
-                {
-                    int mousex = event.mouseButton.x;
-                    int mousey = event.mouseButton.y;
-                    if (active(textx,texty,text.getLocalBounds().width,text.getLocalBounds().height,mousex,mousey))
-                        printf("Button Clicked \n");
+                    int mousex = event.mouseMove.x;
+                    int mousey = event.mouseMove.y;
+                    if (active(startTextx,startTexty,startText.getLocalBounds().width,startText.getLocalBounds().height,mousex,mousey))
+                    {
+                        startText.setColor(sf::Color::Red);
+                    }
                     else
-                        printf("Button Not Clicked \n");
+                    {
+                        startText.setColor(sf::Color::White);
+                    }
+                }
+                if (event.type == sf::Event::MouseButtonPressed)
+                {
+                    if (event.mouseButton.button == sf::Mouse::Left)
+                    {
+                        int mousex = event.mouseButton.x;
+                        int mousey = event.mouseButton.y;
+                        if (active(startTextx,startTexty,startText.getLocalBounds().width,startText.getLocalBounds().height,mousex,mousey))
+                        {
+                            mainScreen = false;
+                            replayText.setColor(sf::Color::White);
+                        }
+                    }
                 }
             }
+
+            // Clear screen
+            app.clear();
+
+            // Draw the sprite
+            app.draw(sprite);
+            app.draw(startText);
         }
+        else //Game Screen Display
+        {
 
-        // Clear screen
-        app.clear();
-
-        // Draw the sprite
-        app.draw(sprite);
-        app.draw(text);
-
+            sf::Event event;
+            while (app.pollEvent(event))
+            {
+                // Close window : exit
+                if (event.type == sf::Event::Closed)
+                    app.close();
+                if (event.type == sf::Event::MouseMoved)
+                {
+                    int mousex = event.mouseMove.x;
+                    int mousey = event.mouseMove.y;
+                    if (active(replayx,replayy,replayText.getLocalBounds().width,replayText.getLocalBounds().height,mousex,mousey))
+                    {
+                        replayText.setColor(sf::Color::Red);
+                    }
+                    else
+                    {
+                        replayText.setColor(sf::Color::White);
+                    }
+                }
+                if (event.type == sf::Event::MouseButtonPressed)
+                {
+                    if (event.mouseButton.button == sf::Mouse::Left)
+                    {
+                        int mousex = event.mouseButton.x;
+                        int mousey = event.mouseButton.y;
+                        if (active(replayx,replayy,replayText.getLocalBounds().width,replayText.getLocalBounds().height,mousex,mousey)){
+                            mainScreen = true;
+                            startText.setColor(sf::Color::White);
+                        }
+                    }
+                }
+            }
+            app.clear();
+            app.draw(ball);
+            app.draw(massText);
+            app.draw(velocityText);
+            app.draw(angleText);
+            app.draw(replayText);
+        }
         // Update the window
         app.display();
     }
