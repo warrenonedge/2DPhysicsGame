9e38dcaeb25bdaded19a278cdc2bfa517e618b14
Jonathan Dimmick <xxtheamazingwebheadxx@gmail.com>
2014-04-07 20:07:39 -0700

    added Point class
    have idea for how to store points

--- Physics.cpp
+++ Physics.cpp
@@ -3,6 +3,33 @@
 
 using namespace std;
 
+Point::Point(double x, double y) {
+    this->x = x;
+    this->y = y;
+}
+
+void Point::setX(double newx) {
+    this->x = newx;
+}
+
+void Point::setY(double newy) {
+    this->y = newy;
+}
+
+void Point::move(double dx, double dy) {
+    this->x += dx;
+    this->y += dy;
+}
+
+double Point::getX() {
+    return this->x;
+}
+
+double Point::getY() {
+    return this->y;
+}
+
+
 World::World(double w, double h, double gf)
 {
     this->width = w;
@@ -35,5 +62,72 @@ double World::getGforce() {
 }
 
 
+Wobject::Wobject(double centerx,double centery,vector<Point> points,double mass,double xaccel,double yaccel)
+{
+    this->centerx = centerx;
+    this->centery = centery;
+    this->mass = mass;
+    this->xaccel = xaccel;
+    this->yaccel = yaccel;
 
+    for(int i=0;i<points.size();i++)
+    {
+        Point p = Point(points[i].getX(),points[i].getY());
+        this.points.push_back(p);
+    }
+}
 
+void Wobject::setCenterX(double newcenterx)
+{
+    this->centerx = newcenterx;
+}
+
+void Wobject::setCenterY(double newcentery)
+{
+    this->centery = newcentery;
+}
+
+void Wobject::setMass(double newmass)
+{
+    this->mass = newmass;
+}
+
+void Wobject::setXAccel(double newxvel)
+{
+    this->xaccel = newxvel;
+}
+
+void Wobject::setYAccel(double newyvel)
+{
+    this->yaccel = newyvel;
+}
+
+double Wobject::getCenterX()
+{
+    return this->centerx;
+}
+
+double Wobject::getCenterY()
+{
+    return this->centery;
+}
+
+double Wobject::getMass()
+{
+    return this->mass;
+}
+
+double* Wobject::getPoints()
+{
+    return this->points;
+}
+
+double Wobject::getXAccel()
+{
+    return this->xaccel;
+}
+
+double Wobject::getYAccel()
+{
+    return this->yaccel;
+}
--- Physics.h
+++ Physics.h
@@ -1,6 +1,20 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
+#include <vector>
 
+class Point {
+    private:
+        double x, y;
+    public:
+        Point(double x, double y);
+        void setX(double newx);
+        void setY(double newy);
+        void move(double dx, double dy);
+
+        double getX(void);
+        double getY(void);
+
+};
 
 class World {
     private:
@@ -16,6 +30,27 @@ class World {
         double getHeight(void);
         double getGforce(void);
 
+};
+
+class Wobject {
+    private:
+        double centerx,centery,mass,xaccel,yaccel;
+        vector<Point> points;
+    public:
+        Wobject(double,double,vector<Point>,double,double,double);
+        void setCenterX(double);
+        void setCenterY(double);
+        void setMass(double);
+        void setXAccel(double);
+        void setYAccel(double);
+
+
+        double getCenterX(void);
+        double getCenterY(void);
+        double getMass(void);
+        double* getPoints(void);
+        double getXAccel(void);
+        double getYAccel(void);
 };
 
 #endif // PHYSICS_H
